b32196d82a5fbcaa5f20bd0084389ed2bd6d7031
rigole <foplacide@gmail.com>
2021-12-28 19:37:32 +0100

    reading the data file of the NASA in csv format

---
+++ Planets-Project/index.js
@@ -0,0 +1,17 @@
+const parse = require('csv-parse')
+const fs = require('fs')
+const results = []
+
+
+fs.createReadStream('kepler_data.csv')
+    .on('data', (data) => {
+        results.push(data)
+    })
+    .on('error', (error)=>{
+        console.log(error)
+    })
+    .on('end', () => {
+        console.log(results)
+        console.log('done')
+    })
+// parse()
---
+++ hello.js
@@ -0,0 +1,8 @@
+const mission = "learn"
+
+if (mission === "learn"){
+    console.log("TIme to code")
+}
+else{
+    console.log(`Is ${mission} really more fun`)
+}

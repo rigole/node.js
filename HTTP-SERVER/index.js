3e82b2d24a9f32992239ab1a3b0885b794c7a57f
rigole <foplacide@gmail.com>
2021-12-29 21:19:28 +0100

    returning html

--- HTTP-SERVER/index.js
+++ HTTP-SERVER/index.js
@@ -9,13 +9,28 @@ const http = require("http");
 const PORT = 3000;
 
 const server = http.createServer((req, res) =>{
-    res.writeHead(200, {
-        'Content-Type': 'application/json',
-    })
-    res.end(JSON.stringify({
-        id:1,
-        name: 'Placide Rigole FOLEU'
-    }))
+        if (req.url === '/friends') {
+           // res.writeHead(200, {
+          //      'Content-Type': 'application/json',
+          //  })
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json ')
+            res.end(JSON.stringify({
+                id:1,
+                name: 'Placide Rigole FOLEU'
+            }))
+         } else if (req.url === '/messages'){
+            res.setHeader('Content-Type', 'text/html')
+            res.write("<html lang='en'>")
+            res.write("<body>")
+            res.write("<ul>")
+            res.write("<li>Hello Placide</li>")
+            res.write("<li>What are your thoughts on Artificial Intelligence?</li>")
+            res.write("</ul>")
+            res.write("</body>")
+            res.write("</html>")
+            res.end()
+        }
 })
 
 server.listen(PORT, () =>{

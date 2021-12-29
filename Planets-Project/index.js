e69ecdfb001c391e8a1e207f8704cdfe1685f5e0
rigole <foplacide@gmail.com>
2021-12-29 13:57:03 +0100

    finding habitable planets on our csv files according to certain criteria

--- Planets-Project/index.js
+++ Planets-Project/index.js
@@ -1,9 +1,13 @@
 import { parse } from "csv-parse";
 import fs from "fs";
 //const fs = require('fs');
-const results = [];
+const isHabitablePlanets = [];
 
 
+
+function isHabitablePlanet(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED' && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11 && planet['koi_prad'] < 1.6
+}
 
 fs.createReadStream('kepler_data.csv')
 
@@ -12,13 +16,16 @@ fs.createReadStream('kepler_data.csv')
         columns: true,
     }))
     .on('data', (data) => {
-        results.push(data)
+        if (isHabitablePlanet(data)){
+            isHabitablePlanets .push(data)
+        }
+
     })
     .on('error', (error)=>{
         console.log(error)
     })
     .on('end', () => {
-        console.log(results)
+        console.log(`${isHabitablePlanets.length} habitable planets found`)
         console.log('done')
     })
 // parse()

600f1d3811c2dd18eac2d194da1359131c627beb
rigole <foplacide@gmail.com>
2021-12-30 20:13:47 +0100

    returning user by id

--- HTTP-SERVER/index.js
+++ HTTP-SERVER/index.js
@@ -1,25 +1,40 @@
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
-
-
-
 const http = require("http");
 //import { http } from "http";
 const PORT = 3000;
 
+const friends = [
+    {
+        id: 0,
+        name: 'Placide FOLEU',
+    },
+
+    {
+        id: 1,
+        name: 'RIGOLE Billionaire',
+    },
+    {
+    id: 2,
+    name: 'RIGOLE Billionaire',
+    }
+]
+
 const server = http.createServer((req, res) =>{
-        if (req.url === '/friends') {
-           // res.writeHead(200, {
-          //      'Content-Type': 'application/json',
-          //  })
+    const items = req.url.split('/')
+        if (items[1] === 'friends') {
+
             res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json ')
-            res.end(JSON.stringify({
-                id:1,
-                name: 'Placide Rigole FOLEU'
-            }))
-         } else if (req.url === '/messages'){
+            res.setHeader('Content-Type',  'application/json ')
+            if (items.length === 3){
+                const friendIndex = Number(items[2])
+                res.end(JSON.stringify(friends[friendIndex]))
+            }else{
+                res.end(JSON.stringify(friends))
+            }
+
+         } else if (items[1] === 'messages'){
             res.setHeader('Content-Type', 'text/html')
             res.write("<html lang='en'>")
             res.write("<body>")

f11e0eb639fdbb050ad9e3b3c49e00c58c8cf1b0
rigole <foplacide@gmail.com>
2022-01-04 23:38:18 +0100

    adding data to our server

--- HTTP-SERVER/index.js
+++ HTTP-SERVER/index.js
@@ -23,32 +23,40 @@ const friends = [
 
 const server = http.createServer((req, res) =>{
     const items = req.url.split('/')
-        if (items[1] === 'friends') {
 
-            res.statusCode = 200;
-            res.setHeader('Content-Type',  'application/json ')
-            if (items.length === 3){
-                const friendIndex = Number(items[2])
-                res.end(JSON.stringify(friends[friendIndex]))
-            }else{
-                res.end(JSON.stringify(friends))
-            }
+    if (req.method === 'POST' && items[1] === 'friends'){
+        req.on('data', (data) => {
+            const friend = data.toString()
+            console.log('Request:', friend)
+            friends.push(JSON.parse(friend))
+        })
+    } else if ( req.method === 'GET' && items[1] === 'friends') {
 
-         } else if (items[1] === 'messages'){
-            res.setHeader('Content-Type', 'text/html')
-            res.write("<html lang='en'>")
-            res.write("<body>")
-            res.write("<ul>")
-            res.write("<li>Hello Placide</li>")
-            res.write("<li>What are your thoughts on Artificial Intelligence?</li>")
-            res.write("</ul>")
-            res.write("</body>")
-            res.write("</html>")
-            res.end()
-        } else {
-            res.statusCode = 404
-            res.end()
+        res.statusCode = 200;
+        res.setHeader('Content-Type',  'application/json ')
+        if (items.length === 3){
+            const friendIndex = Number(items[2])
+            res.end(JSON.stringify(friends[friendIndex]))
+        }else{
+            res.end(JSON.stringify(friends))
         }
+
+    } else if (req.method === 'GET' && items[1] === 'messages'){
+        res.setHeader('Content-Type', 'text/html')
+        res.write("<html lang='en'>")
+        res.write("<body>")
+        res.write("<ul>")
+        res.write("<li>Hello Placide</li>")
+        res.write("<li>What are your thoughts on Artificial Intelligence?</li>")
+        res.write("</ul>")
+        res.write("</body>")
+        res.write("</html>")
+        res.end()
+    } else {
+        res.statusCode = 404
+        res.end()
+    }
+
 })
 
 server.listen(PORT, () =>{

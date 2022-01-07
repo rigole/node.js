61d38c914695795cc6947c853a338dc2e552478a
rigole <foplacide@gmail.com>
2022-01-07 08:58:28 +0100

    friends according to id

--- EXPRESS-PROJECT/server.js
+++ EXPRESS-PROJECT/server.js
@@ -5,17 +5,31 @@ const PORT = 3000;
 
 const friends = [
     {
-        id: 1,
+        id: 0,
         name: "Mpodol Um Nyobe"
     },
     {
-        id: 2,
+        id: 1,
         name: "Ernest Ouandie"
     }
 ]
 
 app.get('/friends', (req, res) => {
     res.json(friends)
+})
+
+app.get('/friends/:friendId', (req, res) => {
+    const friendId = Number(req.params.friendId);
+    const friend = friends[friendId];
+    if (friend){
+        res.status(200).json(friend)
+    } else{
+        res.status(404).json({
+            error: "Friend does not exist"
+        })
+    }
+
+
 })
 
 app.get('/messages', (req, res) => {

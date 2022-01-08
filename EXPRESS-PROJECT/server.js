1c1ff23c71cc159c5da1c45553e3751d93786bba
rigole <foplacide@gmail.com>
2022-01-08 11:11:48 +0100

    addint firends with post

--- EXPRESS-PROJECT/server.js
+++ EXPRESS-PROJECT/server.js
@@ -19,8 +19,30 @@ const friends = [
 ];
 
 app.use((req, res, next) =>{
+    const start = Date.now();
     console.log(`${req.method} ${req.url}`)
     next()
+    const delta = Date.now() - start;
+    console.log(`${req.method} ${req.url} ${delta}ms`)
+})
+
+app.use(express.json())
+
+app.post('/friends', (req, res) => {
+
+        if (!req.body.name){
+            res.status(400).json({
+                error: 'Missing friend name'
+            });
+        }
+
+
+        const newFriend = {
+            name: req.body.name,
+            id: friends.length
+        }
+        friends.push(newFriend)
+        res.json(newFriend)
 })
 
 app.get('/friends', (req, res) => {

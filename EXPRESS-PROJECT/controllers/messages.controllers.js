4324486cc204080d7d09bbd5df0d9e32cca18dd3
rigole <foplacide@gmail.com>
2022-01-13 21:46:46 +0100

    returning file as a request request result in message

--- EXPRESS-PROJECT/controllers/messages.controllers.js
+++ EXPRESS-PROJECT/controllers/messages.controllers.js
@@ -1,5 +1,9 @@
+const path = require('path')
+
 function getMessages  (req, res) {
-    res.send('<ul><li>Hello UM Nyobe</li></ul>')
+
+    //res.send('<ul><li>Hello UM Nyobe</li></ul>')
+    res.sendFile(path.join(__dirname, '..', 'public', 'pp.jpg'))
 }
 
 function postMessages (req, res) {

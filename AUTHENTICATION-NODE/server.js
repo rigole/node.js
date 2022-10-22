3392e369ce8d39fb0b073b7014a57a2a0ab1f461
rigole <foplacide@gmail.com>
2022-10-22 17:17:16 +0100

    setting the OpenSSL and the server file

---
+++ AUTHENTICATION-NODE/server.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const path = require('path')
+const https = require('https')
+const express = require("express")
+
+const PORT = 3000;
+
+const app = express();
+
+app.get('/secret', (req, res) => {
+    return res.send("Your personal secret value is 14");
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+
+https.createServer({
+    key: fs.readFileSync("key.pem"),
+    cert: fs.readFileSync("cert.pem"),
+}, app).listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`)
+})
+
+
+/*app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`)
+})*/

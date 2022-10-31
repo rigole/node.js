de3cb07c21ca8feba349ae4dd7da352d79c2e9c3
rigole <foplacide@gmail.com>
2022-10-31 15:40:59 +0100

    dotenv to avoid sharing secret key

--- AUTHENTICATION-NODE/server.js
+++ AUTHENTICATION-NODE/server.js
@@ -7,9 +7,30 @@ const express = require("express")
 const PORT = 3000;
 
 const app = express();
-
+require('dotenv').config();
 app.use(helmet())
-app.get('/secret', (req, res) => {
+
+const config = {
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET
+}
+
+
+function checkLoggedIn (req, res, next) {
+    const isLoggedIn = true;
+    if(isLoggedIn) {
+        return res.status(401).json({
+            error: "You must log in!"
+        });
+    }
+    next();
+}
+
+app.get('/auth/google',(req, res) => {})
+app.get('/auth/google/callback', (req, res) => {})
+app.get('/auth/logout', (req, res) => {})
+
+app.get('/secret',checkLoggedIn, (req, res) => {
     return res.send("Your personal secret value is 14");
 })
 
